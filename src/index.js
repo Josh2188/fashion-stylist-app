1fb1eed38f66a9099ce93f9dc3a8845d98e98401
Josh2188 <xxtredszewsazfdxsxx@gmail.com>
2025-08-25 00:07:28 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// 我們將 serviceWorkerRegistration 引入的方式稍微改變
+import { unregister } from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +11,12 @@ root.render(
   </React.StrictMode>
 );
 
-// This function starts the PWA service worker.
-serviceWorkerRegistration.register();
+// 關鍵的修改在這裡：
+// 我們不再呼叫 register()，因為我們的 service-worker.js 是自訂的。
+// Vercel 會自動處理 PWA 的註冊，所以我們不需要在這裡做任何事情。
+// 如果您未來想取消 PWA 功能，可以呼叫 unregister()。
+// For now, we can comment this out or call unregister if needed.
+// unregister();
+
+// 我們將手動在 public/index.html 中載入 service worker，
+// 所以這裡不需要再做任何註冊。
